0f371db32d02c57f35532dee81cf1d0c2b543feb
LaNice Powell <lanicemp@aol.com>
2019-07-08 02:12:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,37 @@
 # My Code here....
+def map_to_negativize(source_array)
+num = [1, 2, 3, -9].map{ |num| num * -1 }
+end 
+
+def map_to_no_change(source_array)
+  dune = ["paul", "gurney", "vladimir", "jessica", "chani"]
+end
+
+def map_to_double(source_array)
+num = [1, 2, 3, -9].map{ |num| num * 2 }
+end 
+
+def map_to_square(source_array)
+num = [1, 2, 3, -9].map! { |num| num ** 2 }  
+end 
+
+def reduce_to_total(source_array, starting_point=0)
+  [1, 2, 3].reduce(100){ |total, num| total + num }
+end 
+
+def reduce_to_total(source_array, starting_point)
+  [1, 2, 3].reduce(100){ |total, num| total + num }
+end 
+
+def reduce_to_all_true(source_array)
+  [1, 2, 3].reduce(0){ |total, num| total + num }
+end 
+
+def reduce_to_any_true(source_array)
+   [1, 2, true, "razmatazz"].reduce(3){ |total, num| total + num }
+end
+
+
+
+
+
