b459009f95257c0563b58200c953b3dd0e8b5a88
LaNice Powell <lanicemp@aol.com>
2019-07-08 02:14:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -24,7 +24,7 @@ def reduce_to_total(source_array, starting_point)
 end 
 
 def reduce_to_all_true(source_array)
-  [1, 2, 3].reduce(0){ |total, num| total + num }
+  source_array.all?
 end 
 
 def reduce_to_any_true(source_array)
